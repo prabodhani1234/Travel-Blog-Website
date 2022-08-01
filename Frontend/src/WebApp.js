73d8248c888814ab83d5eb9bbc3aad2cf8d8b315
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2022-08-01 11:48:09 +0530

    initial commit

---
+++ Frontend/src/WebApp.js
@@ -0,0 +1,9 @@
+function WebApp() {
+  return (
+    <div className="Travel app">
+      Travel and Tour
+    </div>
+  );
+}
+
+export default WebApp;
---
+++ Frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import WebApp from "./WebApp";
+
+ReactDOM.render(
+  <React.StrictMode>  
+      <WebApp />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
