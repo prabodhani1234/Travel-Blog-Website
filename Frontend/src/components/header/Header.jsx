6ed528d47237b4b6d8aa90a602334c8dac5f0d5e
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2022-08-02 00:39:49 +0530

    design header of the home screen

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -3,7 +3,15 @@ import "./header.css";
 export default function Header() {
   return (
     <div className="header">
-      Header
+        <div className="headerTitles">
+            <span className="headerTitleSm">Travel & Tour</span>
+            <span className="headerTitleLg">Amazing Places on Earth</span>
+        </div>
+        <img
+            className="headerImg"
+            src="https://images.pexels.com/photos/4992803/pexels-photo-4992803.jpeg?cs=srgb&dl=pexels-rachel-claire-4992803.jpg&fm=jpg"
+            alt=""
+        />
     </div>
   );
 }
